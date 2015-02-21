9ae3f08917c1f319e7858ae1ae84f86698d9067e
Samgineer <samgineer@gmail.com>
2015-02-21 14:09:16 -0600

    Have my std's

---
+++ app/view/employees/manage.php
@@ -0,0 +1,10 @@
+<?php
+use Sm\Core\Abstraction\IoC;
+
+/** @var \Model\User $user */
+if ($user = IoC::$session->get('user')) {
+    $groups_arr = $user->findGroups();
+    $current_group = array_shift($groups_arr);
+    var_dump($current_group);
+}
+?>
