75213fb706506291bc6c91ed5278873bf5708ea2
Samgineer <samgineer@gmail.com>
2015-02-21 15:48:44 -0600

    Have my std's

--- app/controller/backend/add_charity.php
+++ app/controller/backend/add_charity.php
@@ -4,7 +4,9 @@
  * Date: 2/21/2015
  * Time: 1:54 PM
  */
+use Model\Entity;
 use Model\Group;
+use Model\GroupEntityMap;
 use Model\GroupGroupMap;
 use Model\Password;
 use Model\User;
@@ -16,26 +18,40 @@ $func = function ($args) {
     $phone_number = isset($args['phone_number']) ? $args['phone_number'] : null;
     $tax_code = isset($args['tax_code']) ? $args['tax_code'] : null;
     $last_name = isset($args['last_name']) ? $args['last_name'] : null;
+    $url = isset($args['url']) ? $args['url'] : null;
 
     $contact_name = isset($args['contact_name']) ? $args['contact_name'] : null;
     $description = isset($args['description']) ? $args['description'] : null;
     if ($name != null):
         /** @var Group $session_group */
         $session_group = IoC::$session->get('group');
-        if ($session_group == false) {
+        /** @var Group $session_group */
+        if (!$session_group) {
             return false;
         }
-        $user_settings = ['name' => $name, 'description' => $description, 'type' => 2];
+        $time = new DateTime();
+        $time->getTimestamp();
+        $user_settings = ['name' => $name, 'description' => $description, 'type' => 2, 'alias' => $name . $time->getTimestamp()];
         $group = new Group();
         $group->set($user_settings)->create();
-        var_dump($group);
+
+        $entity = new Entity();
+        $entity->set(['address' => $address, 'phone_number' => $phone_number, 'url' => $url, 'description' => $description, 'contact_name' => $contact_name, 'tax_code' => $tax_code])->create();
+
         $group_id = $group->getId();
         if (!$group_id) {
             return false;
         }
-
+        $id = $entity->getId();
+        if ($id) {
+            $group_entity_map = new GroupEntityMap();
+            $group_entity_map->addRow($group_id, $id);
+        }
         $g_g_map = new GroupGroupMap();
         $g_g_map->addRow($group_id, $session_group->getId());
+        $group->findEntity();
+        //var_dump($group);
+
     endif;
 
     var_dump($args);
