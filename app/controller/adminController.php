a844e4d4f2741baa9a45695c7a817c19b83c319a
Samgineer <samgineer@gmail.com>
2015-02-21 14:48:59 -0600

    Have my std's

---
+++ app/controller/adminController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: Samuel
+ * Date: 2/21/2015
+ * Time: 1:47 PM
+ */
+namespace Controller;
+
+use Sm\Core\Abstraction\IoC;
+
+class employeesController extends BaseController {
+    public function manage() {
+        $view = &IoC::$view;
+        $this->set_template();
+        $view->setViewData(['title' => 'General Test of settings', 'secondary_title' => 'My Profile']);
+        $view->create('employees/manage', [], 'manage');
+        $view->nest_view_named('template', 'manage', 'body');
+    }
+
+    public function _add_html() {
+        $view = &IoC::$view;
+
+        $view->setViewData(['title' => 'Add Employee', 'secondary_title' => 'My Profile']);
+        $view->create('modules/add_empl', [], 'manage');
+        $view->set('manage');
+    }
+
+
+    public function _add() {
+        $sam = $_POST;
+        IoC::$filter->std($sam);
+        $result = IoC::$backend->run('add_employee', [$_POST]);
+        if ($result === true) {
+            #Response::redirect(Link::url('me'));
+        }
+        return $result;
+    }
+
+    public function _file_upload() {
+        $result = IoC::$backend->run('file_upload', ['files' => $_FILES]);
+        if ($result === true) {
+            #Response::redirect(Link::url('me'));
+        }
+        return $result;
+    }
+}
