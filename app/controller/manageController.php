3dc1bd4a3dd7cd232a528e4fe31506a9bc504c16
Samgineer <samgineer@gmail.com>
2015-02-21 15:06:21 -0600

    Have my std's

---
+++ app/controller/manageController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Samuel
+ * Date: 2/21/2015
+ * Time: 1:47 PM
+ */
+namespace Controller;
+
+use Sm\Core\Abstraction\IoC;
+
+class manageController extends BaseController {
+    public function manage() {
+        $view = &IoC::$view;
+        $this->set_template();
+        $view->setViewData(['title' => 'General Test of settings', 'secondary_title' => 'My Profile']);
+        $view->create('employees/manage', [], 'manage');
+        $view->nest_view_named('template', 'manage', 'body');
+    }
+
+    public function add_charity() {
+        $view = &IoC::$view;
+        $this->set_template();
+        $view->setViewData(['title' => 'General Test of settings', 'secondary_title' => 'My Profile']);
+        $view->create('employees/manage', [], 'manage');
+        $view->nest_view_named('template', 'manage', 'body');
+    }
+
+    public function _add_charity() {
+        $view = &IoC::$view;
+        $this->set_template();
+        $view->setViewData(['title' => 'General Test of settings', 'secondary_title' => 'My Profile']);
+        $view->create('employees/manage', [], 'manage');
+        $view->nest_view_named('template', 'manage', 'body');
+    }
+
+}
---
+++ app/view/modules/add_empl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Samuel
+ * Date: 2/21/2015
+ * Time: 1:54 PM
+ */
+use Model\Password;
+use Model\User;
+
+$func = function ($args) {
+    $args = $args[0];
+    $name = isset($args['name']) ? $args['name'] : null;
+    $address = isset($args['address']) ? $args['address'] : null;
+    $contact_name = isset($args['contact_name']) ? $args['contact_name'] : null;
+    $url = isset($args['password']) ? $args['password'] : null;
+    $tax_code = isset($args['tax_code']) ? $args['tax_code'] : null;
+    $phone_number = isset($args['phone_number']) ? $args['phone_number'] : null;
+    if ($name != null):
+        $user_settings = ['name' => $name, 'address' => $address, 'contact_name' => $contact_name, 'tax_code' => $tax_code, 'type' => 1, 'phone_number' => $phone_number];
+        $user = new User();
+        $user->set($user_settings)->create();
+        var_dump($user);
+        $user_id = $user->getId();
+        if (!$user_id) {
+            return false;
+        }
+        Password::add($user_id, $url);
+        $user->make_directories($user->getUsername());
+    endif;
+
+    var_dump($args);
+    return true;
+};
